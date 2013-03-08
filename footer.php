1c78e4e138183dfa742a3f48608b564f2dd4e7ff
tinyhill <tinyhill@163.com>
2013-03-08 13:53:41 +0800

    reformat code

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <?php global $options; ?>
+</div>
+<div class="footer">
+	<div class="copyright">COPYRIGHT &copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url'); ?>"><?php echo strtoupper(str_replace('www.', '', $_SERVER['HTTP_HOST'])); ?></a> - THEME BY <a target="_blank" href="http://www.mangguo.org/">芒果小站</a></div>
+	<div class="about clearfix">
+		<ul class="sitemap clearfix">
+<?php wp_nav_menu(array('theme_location' => 'sitemap', 'container' => false, 'menu_class' => false, 'menu_id' => false, 'items_wrap' => '%3$s')); ?>
+		</ul>
+		<a href="<?php bloginfo('url'); ?>" class="licence">&copy; <?php bloginfo('name'); ?></a>
 	</div>
-	<div class="footer">
-		<div class="copyright">COPYRIGHT &copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url'); ?>"><?php echo strtoupper(str_replace('www.', '', $_SERVER['HTTP_HOST'])); ?></a> - THEME BY <a target="_blank" href="http://www.mangguo.org/">芒果小站</a></div>
-		<div class="about clearfix">
-			<ul class="sitemap clearfix">
-	<?php wp_nav_menu(array('theme_location' => 'sitemap', 'container' => false, 'menu_class' => false, 'menu_id' => false, 'items_wrap' => '%3$s')); ?>
-			</ul>
-			<a href="<?php bloginfo('url'); ?>" class="licence">&copy; <?php bloginfo('name'); ?></a>
-		</div>
-	</div>
+</div>
 </div>
 <?php if ($options['minify']) : ?>
 <script charset="utf-8" src="<?php bloginfo('template_url');?>/min/?b=<?php echo str_replace(get_bloginfo('home') . '/', '', get_bloginfo('template_directory')); ?>/assets&f=jquery.min.js,jquery.cookie.js,mangguo.js"></script>
--- header.php
+++ header.php
@@ -19,59 +19,59 @@
 </head>
 <body>
 <div class="page">
-	<div class="header">
-		<h1 class="logo"><a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></h1>
-		<ul class="quick-menu clearfix">
-			<li><span>欢迎光临<?php if($_COOKIE['comment_author_' . COOKIEHASH] != '') echo '，<b>' . $_COOKIE['comment_author_'.COOKIEHASH] . '</b>'; ?></span></li>
-			<li class="subscribe"><a href="<?php bloginfo('rss2_url'); ?>" target="_blank">订阅</a></li>
-			<li class="random"><?php $random = get_posts('numberposts=1&orderby=rand'); foreach($random as $post) : ?><a href="<?php the_permalink(); ?>">随机</a><?php endforeach; ?></li>
+<div class="header">
+	<h1 class="logo"><a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></h1>
+	<ul class="quick-menu clearfix">
+		<li><span>欢迎光临<?php if($_COOKIE['comment_author_' . COOKIEHASH] != '') echo '，<b>' . $_COOKIE['comment_author_'.COOKIEHASH] . '</b>'; ?></span></li>
+		<li class="subscribe"><a href="<?php bloginfo('rss2_url'); ?>" target="_blank">订阅</a></li>
+		<li class="random"><?php $random = get_posts('numberposts=1&orderby=rand'); foreach($random as $post) : ?><a href="<?php the_permalink(); ?>">随机</a><?php endforeach; ?></li>
+	</ul>
+</div>
+<div class="content">
+	<div id="slide" class="slide<?php if (isset($_COOKIE['slide_mini'])) echo ' slide-mini'; ?>">
+		<ol class="slide-content">
+<?php foreach ($options['slide_links'] as $k => $v) : ?>
+			<li style="<?php if ($k == 0) echo 'display:block;'; else echo 'display:none;'; ?>">
+				<h3><a target="_blank" href="<?php echo $v[2]; ?>"><?php echo $v[0]; ?></a></h3>
+				<p><?php echo $v[1]; ?></p>
+				<p><a target="_blank" href="<?php echo $v[2]; ?>">猛击这里查看</a></p>
+			</li>
+<?php endforeach; ?>
+		</ol>
+		<ul class="slide-nav clearfix">
+<?php foreach ($options['slide_links'] as $k => $v) : ?>
+			<li<?php if ($k == 0) echo ' class="active"'; ?>><?php echo $k + 1; ?></li>
+<?php endforeach; ?>
+		</ul>
+		<span class="prev"></span>
+		<span class="next"></span>
+	</div>
+	<div id="slide-toggle" class="slide-toggle clearfix">
+<?php if (isset($_COOKIE['slide_mini'])) : ?>
+		<span class="toggle toggle-mini">切换到完整模式</span>
+<?php else : ?>
+		<span class="toggle">切换到精简模式</span>
+<?php endif; ?>
+	</div>
+	<div class="search">
+		<div class="message"><b>他说</b>：<?php bloginfo('description'); ?></div>
+		<form action="/" method="get" class="searchform clearfix">
+			<span class="s"><input type="text" name="s" id="s" value="<?php the_search_query(); ?>"></span><span class="searchsubmit"><button type="submit"></button></span>
+		</form>
+	</div>
+	<div class="tab">
+		<ul class="clearfix">
+<?php wp_nav_menu(array('theme_location' => 'tab', 'container' => false, 'menu_class' => false, 'menu_id' => false, 'items_wrap' => '%3$s')); ?>
 		</ul>
 	</div>
-	<div class="content">
-		<div id="slide" class="slide<?php if (isset($_COOKIE['slide_mini'])) echo ' slide-mini'; ?>">
-			<ol class="slide-content">
-	<?php foreach ($options['slide_links'] as $k => $v) : ?>
-				<li style="<?php if ($k == 0) echo 'display:block;'; else echo 'display:none;'; ?>">
-					<h3><a target="_blank" href="<?php echo $v[2]; ?>"><?php echo $v[0]; ?></a></h3>
-					<p><?php echo $v[1]; ?></p>
-					<p><a target="_blank" href="<?php echo $v[2]; ?>">猛击这里查看</a></p>
-				</li>
-	<?php endforeach; ?>
-			</ol>
-			<ul class="slide-nav clearfix">
-	<?php foreach ($options['slide_links'] as $k => $v) : ?>
-				<li<?php if ($k == 0) echo ' class="active"'; ?>><?php echo $k + 1; ?></li>
-	<?php endforeach; ?>
+	<div class="quick-link">
+		<div class="text-links">
+<?php foreach ($options['text_links'] as $k => $v) : if ($k != 0) echo '<s>/</s>'; ?><a target="_blank" href="<?php echo $v[2]; ?>" style="color:<?php echo $v[1]; ?>;"><?php echo $v[0]; ?></a><?php endforeach; ?>
+		</div>
+		<div class="clearfix">
+			<ul class="category-list clearfix">
+<?php $args = array('orderby' => 'count', 'order' => 'DESC', 'hide_empty' => false, 'title_li' => ''); wp_list_categories($args); ?>
 			</ul>
-			<span class="prev"></span>
-			<span class="next"></span>
+			<div class="counter">标签数目统计值 <a href="/tags/"><?php $counter = wp_count_terms('post_tag'); $terms = str_split($counter); foreach ($terms as $term) echo '<b>' . $term . '</b>'; ?></a></div>
 		</div>
-		<div id="slide-toggle" class="slide-toggle clearfix">
-	<?php if (isset($_COOKIE['slide_mini'])) : ?>
-			<span class="toggle toggle-mini">切换到完整模式</span>
-	<?php else : ?>
-			<span class="toggle">切换到精简模式</span>
-	<?php endif; ?>
-		</div>
-		<div class="search">
-			<div class="message"><b>他说</b>：<?php bloginfo('description'); ?></div>
-			<form action="/" method="get" class="searchform clearfix">
-				<span class="s"><input type="text" name="s" id="s" value="<?php the_search_query(); ?>"></span><span class="searchsubmit"><button type="submit"></button></span>
-			</form>
-		</div>
-		<div class="tab">
-			<ul class="clearfix">
-	<?php wp_nav_menu(array('theme_location' => 'tab', 'container' => false, 'menu_class' => false, 'menu_id' => false, 'items_wrap' => '%3$s')); ?>
-			</ul>
-		</div>
-		<div class="quick-link">
-			<div class="text-links">
-	<?php foreach ($options['text_links'] as $k => $v) : if ($k != 0) echo '<s>/</s>'; ?><a target="_blank" href="<?php echo $v[2]; ?>" style="color:<?php echo $v[1]; ?>;"><?php echo $v[0]; ?></a><?php endforeach; ?>
-			</div>
-			<div class="clearfix">
-				<ul class="category-list clearfix">
-	<?php $args = array('orderby' => 'count', 'order' => 'DESC', 'hide_empty' => false, 'title_li' => ''); wp_list_categories($args); ?>
-				</ul>
-				<div class="counter">标签数目统计值 <a href="/tags/"><?php $counter = wp_count_terms('post_tag'); $terms = str_split($counter); foreach ($terms as $term) echo '<b>' . $term . '</b>'; ?></a></div>
-			</div>
-		</div>
+	</div>
